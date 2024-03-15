2f872b8f11d3c2d385dcaa87e99018eae34396a1
Hafiz Masood <masood.funsoltech@gmail.com>
2024-03-15 15:04:15 +0500

    sgk

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,6 +7,8 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
      setContentView(R.layout.activity_main)
 
 //kjkljl
